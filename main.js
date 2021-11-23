558a1fada6b0986215507fd2c76253c10e02959e
Med-dahreddine <dahreddine.med2@gmail.com>
2021-11-23 13:50:20 +0100

    First CSS

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-
+// DOM
+const touches = [...document.querySelectorAll(".bouton")];
+const listKeycode = touches.map((touche) => touche.dataset.key);
+document.addEventListener("keydown", ({ keyCode }) => console.log(keyCode));
